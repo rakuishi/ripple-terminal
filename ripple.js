5ad55c14970f02a3644469052c9ee34686503fdf
rakuishi <rakuishi@gmail.com>
2018-04-05 21:41:25 +0900

    Add `new` command

--- ripple.js
+++ ripple.js
@@ -4,26 +4,34 @@
 
 const RippleAPI = require('ripple-lib').RippleAPI;
 const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' });
-const command = process.argv[2]
 
-const balance = () => {
-  const address = process.argv[3];
-  if (!address) {
-    console.log('Error: Unable to get a ripple address.')
-    return;
-  }
-
-  api.connect().then(() => {
-    api.getAccountInfo(address).then(info => {
-      console.log('xrpBalance: ' + info.xrpBalance);
-      process.exit();
+const operator = {
+  getBalance: () => {
+    const address = process.argv[3];
+    if (!address) {
+      console.log('Error: Unable to get a ripple address.')
+      return;
+    }
+  
+    api.connect().then(() => {
+      api.getAccountInfo(address).then(info => {
+        console.log('xrpBalance: ' + info.xrpBalance);
+        process.exit();
+      });
     });
-  });
-}
+  },
+  generateAddress: () => {
+    const address = api.generateAddress();
+    console.log(address);
+  },
+};
 
-switch (command) {
+switch (process.argv[2]) {
   case 'balance':
-    balance();
+    operator.getBalance();
+    break;
+  case 'new':
+    operator.generateAddress();
     break;
   case 'help':
   default:
@@ -32,6 +40,7 @@ switch (command) {
       '\n' +
       'Available Commands:\n' +
       '  balance   Get XRP balance | ./ripple.js balance [address]\n' +
-      '  help      Help about any command\n'
+      '  help      Help about any command\n' +
+      '  new       Generate a new XRP Ledger address and corresponding secret'
     );
 }
