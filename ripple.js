d578fc1bfe3176a86ca7d40f6628a4b7e52ac4a1
rakuishi <rakuishi@gmail.com>
2018-04-05 20:51:00 +0900

    Add `balance` command

--- ripple.js
+++ ripple.js
@@ -2,15 +2,36 @@
 
 'use strict'
 
+const RippleAPI = require('ripple-lib').RippleAPI;
+const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' });
 const command = process.argv[2]
 
+const balance = () => {
+  const address = process.argv[3];
+  if (!address) {
+    console.log('Error: Unable to get a ripple address.')
+    return;
+  }
+
+  api.connect().then(() => {
+    api.getAccountInfo(address).then(info => {
+      console.log('xrpBalance: ' + info.xrpBalance);
+      process.exit();
+    });
+  });
+}
+
 switch (command) {
+  case 'balance':
+    balance();
+    break;
   case 'help':
   default:
     console.log('Usage:\n' +
       '  ./ripple.js [command]\n' +
       '\n' +
       'Available Commands:\n' +
-      '  help     Help about any command\n'
+      '  balance   Get XRP balance | ./ripple.js balance [address]\n' +
+      '  help      Help about any command\n'
     );
 }
