3d116cd433149304e2ff847e0ed9b6cbafceb154
rakuishi <rakuishi@gmail.com>
2018-04-05 22:50:45 +0900

    Add `fee` command

--- ripple.js
+++ ripple.js
@@ -6,6 +6,7 @@ const RippleAPI = require('ripple-lib').RippleAPI;
 const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' });
 
 const operator = {
+  // balance
   getBalance: () => {
     const address = process.argv[3];
     if (!address) {
@@ -15,11 +16,21 @@ const operator = {
   
     api.connect().then(() => {
       api.getAccountInfo(address).then(info => {
-        console.log('xrpBalance: ' + info.xrpBalance);
+        console.log('XRP Balance: ' + info.xrpBalance);
         process.exit();
       });
     });
   },
+  // fee
+  getFee: () => {
+    api.connect().then(() => {
+      api.getFee().then(fee => {
+        console.log('Transaction fee: ' + fee);
+        process.exit();
+      });
+    });
+  },
+  // new
   generateAddress: () => {
     const address = api.generateAddress();
     console.log(address);
@@ -29,6 +40,9 @@ const operator = {
 switch (process.argv[2]) {
   case 'balance':
     operator.getBalance();
+    break;
+  case 'fee':
+    operator.getFee();
     break;
   case 'new':
     operator.generateAddress();
@@ -40,6 +54,7 @@ switch (process.argv[2]) {
       '\n' +
       'Available Commands:\n' +
       '  balance   Get XRP balance | ./ripple.js balance [address]\n' +
+      '  fee       Get the estimated transaction fee\n' +
       '  help      Help about any command\n' +
       '  new       Generate a new XRP Ledger address and corresponding secret'
     );
