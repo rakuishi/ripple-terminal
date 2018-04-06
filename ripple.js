e64cb23d868b9ee4513e3d3741d8e6cb76a4c517
rakuishi <rakuishi@gmail.com>
2018-04-06 21:41:05 +0900

    Refine ripple.js

--- ripple.js
+++ ripple.js
@@ -5,47 +5,47 @@
 const RippleAPI = require('ripple-lib').RippleAPI;
 const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' });
 
-const operator = {
-  // balance
-  getBalance: () => {
-    const address = process.argv[3];
-    if (!address) {
-      console.log('Error: Unable to get a ripple address.')
-      return;
-    }
-  
-    api.connect().then(() => {
-      api.getAccountInfo(address).then(info => {
-        console.log('XRP Balance: ' + info.xrpBalance);
-        process.exit();
-      });
+// balance
+const getBalance = () => {
+  const address = process.argv[3];
+  if (!address) {
+    console.log('Error: Unable to get a ripple address.');
+    return;
+  }
+
+  api.connect().then(() => {
+    api.getAccountInfo(address).then(info => {
+      console.log('XRP Balance: ' + info.xrpBalance);
+      process.exit(0);
     });
-  },
-  // fee
-  getFee: () => {
-    api.connect().then(() => {
-      api.getFee().then(fee => {
-        console.log('Transaction fee: ' + fee);
-        process.exit();
-      });
+  });
+};
+
+// fee
+const getFee = () => {
+  api.connect().then(() => {
+    api.getFee().then(fee => {
+      console.log('Transaction fee: ' + fee);
+      process.exit(0);
     });
-  },
-  // new
-  generateAddress: () => {
-    const address = api.generateAddress();
-    console.log(address);
-  },
+  });
+};
+
+// new
+const generateAddress = () => {
+  const address = api.generateAddress();
+  console.log(address);
 };
 
 switch (process.argv[2]) {
   case 'balance':
-    operator.getBalance();
+    getBalance();
     break;
   case 'fee':
-    operator.getFee();
+    getFee();
     break;
   case 'new':
-    operator.generateAddress();
+    generateAddress();
     break;
   case 'help':
   default:
